17a0737722ec6476c5baf0f4ac03aa19bdddfb73
lowfront <fronterior@hotmail.com>
2022-05-17 00:01:33 +0900

    add quick sort with memory

--- 220515-sort-algorithm/main.ts
+++ 220515-sort-algorithm/main.ts
@@ -70,10 +70,55 @@ function mergeSort(array: number[]) {
   }
 }
 
+
+/*
+[[5, 3, 8, 1, 2, 4, 9, 7, 6, 5, 3, 0]]
+[[3, 1, 2, 4, 3, 0], 5, [8, 9, 7, 6, 5]]
+[[1, 2, 0], 3, [4, 3], 5, [7, 6, 5], 8, [9]]
+[[0], 1, [2], 3, [3], 4, 5, [5, 6], 7, 8, 9]
+[0, 1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9]
+*/
+function quickSort(array: number[]) {
+  const stack: (number[]|number)[][] = [[array]];
+  let target: (number[]|number)[];
+  let pivot: number;
+  let noArray = true;
+  while (target = stack.shift()) {
+    noArray = true;
+    const newStackItem: (number[]|number)[] = [];
+    for (let i = 0; i < target.length; i++) {
+      const item = target[i];
+      if (!Array.isArray(item)) {
+        newStackItem.push(item);
+        continue;
+      }
+
+      pivot = item.shift(); // first pivot
+      if (!item.length) {
+        newStackItem.push(pivot);
+        continue;
+      }
+      noArray = false;
+
+      const left: number[] = [];
+      const right: number[] = [];
+      for (const n of item) {
+        if (n < pivot) left.push(n);
+        else right.push(n);
+      }
+      if (left.length) newStackItem.push(left);
+      newStackItem.push(pivot);
+      if (right.length) newStackItem.push(right);
+    }
+    if (noArray) return newStackItem;
+    stack.push(newStackItem);
+  }
+}
+
 const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 (async () => {
-  const bubbleSortIterable = bubbleSort(array);
+  // const bubbleSortIterable = bubbleSort(array);
 
   // for (const round of taskRunner(iterable, 1000)) {
   //   (document.getElementById('round') as HTMLElement).textContent = round + '';
@@ -81,6 +126,6 @@ const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
   // }
   // console.log(array)
 
-  const mergeSortedArray = mergeSort(Array.from(Array(101), () => Math.floor(Math.random() * 1000)));
-  console.log('mergeSortedArray', mergeSortedArray);
+  // const mergeSortedArray = mergeSort(Array.from(Array(101), () => Math.floor(Math.random() * 1000)));
+  // console.log('mergeSortedArray', mergeSortedArray);
 })();
