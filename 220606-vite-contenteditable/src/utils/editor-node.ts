2cabf42ce29cb72719b818a348c834457b45fe2e
Low Front <lowfronterior@gmail.com>
2022-06-12 14:48:31 +0900

    feat: adding pre parse

--- 220606-vite-contenteditable/src/utils/editor-node.ts
+++ 220606-vite-contenteditable/src/utils/editor-node.ts
@@ -8,6 +8,7 @@ import { createDFSIterable } from "../helper/utils";
 - Block 노드의 block 속성은 가장 끝에 있는 자식 노드로 BR 태그를 만나면 무효화(줄넘김 방지) 된다.
 - BR 태그는 트리파싱의 다음 순서 노드에게 block 속성을 부여한다.
 - Block 속성의 태그는 다음 순서 노드에게 block 속성을 부여한다.
+- PRE 태그는 모든 속성 무시하고 개행문자(\n) 기준으로 텍스트와 노드 분할
 
 <span>
   123
@@ -21,6 +22,7 @@ import { createDFSIterable } from "../helper/utils";
 */
 
 export const BLOCK_TAGS = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'ADDRESS', 'ARTICLE', 'ASIDE', 'BLOCKQUOTE', 'CANVAS', 'DD', 'DIV', 'DL', 'DT', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FOOTER', 'FORM', 'HEADER', 'HR', 'LI', 'MAIN', 'NAV', 'NOSCRIPT', 'OL', 'P', 'PRE', 'SECTION', 'TABLE', 'TFOOT', 'UL', 'VIDEO'];
+export const PRE_TAGS = ['PRE', 'CODE'];
 export const START_FRAGMENT = '<!--StartFragment-->';
 export const END_FRAGMENT = '<!--EndFragment-->';
 
@@ -48,6 +50,61 @@ export function pasteParse(str: string) {
 
   return wrap;
 }
+export function getChildNodesByPre(editorNode: EditorNode) {
+  const { node, root, block } = editorNode;
+  const strings = node.textContent?.split('\n') ?? [];
+
+  const childNodes = strings.map((str, i) => {
+    return {
+      node: document.createTextNode(str.replaceAll(' ', '&nbsp;')),
+      block: i !== 0,
+      root: i === 0,
+      text: true,
+    };
+  });
+
+  return childNodes;
+}
+
+export function getChildNodesByWithoutPre(editorNode: EditorNode) {
+  const { node, root, block } = editorNode;
+
+  let prevBr: boolean = false;
+  let prevBlockTag: boolean = false;
+
+  const childNodes = [...node.childNodes].map((childNode, index, array) => {
+    const isRoot = root && !index;
+    const isBlockByParent = !root && block && !index;
+
+    const isPrevBlockTag = prevBlockTag;
+    const isPrevBr = prevBr;
+    
+    prevBlockTag = prevBr = false;
+    prevBlockTag ||= isHTMLElement(childNode) && BLOCK_TAGS.includes(childNode.tagName);
+    prevBr ||= isHTMLElement(childNode) && childNode.tagName === 'BR';
+
+    const result = {
+      node: childNode,
+      block: !isRoot && (isBlockByParent || isPrevBlockTag || isPrevBr),
+      root: isRoot,
+      text: isTextNode(childNode),
+    };
+    return result;
+  });
+
+  if (!childNodes.length) {
+    return [
+      {
+        node: document.createTextNode(''),
+        block: !root && block,
+        root,
+        text: true,
+      }
+    ];
+  }
+
+  return childNodes;
+}
 
 export function* nodeToEditorNodes(root: Node) {
   const editorNodeIterator = createDFSIterable<EditorNode>(
@@ -60,51 +117,13 @@ export function* nodeToEditorNodes(root: Node) {
     editorNode => {
       const { node, root, block } = editorNode;
       if (isHTMLElement(node)) {
-        let prevBr: boolean = false;
-        let prevBlockTag: boolean = false;
+        const isPre = PRE_TAGS.includes(node.tagName);
 
-        const childNodes = [...node.childNodes].map((childNode, index, array) => {
-          const isRoot = root && !index;
-          const isBlockByParent = !root && block && !index;
-
-          const isPrevBlockTag = prevBlockTag;
-          const isPrevBr = prevBr;
-          
-          prevBlockTag = prevBr = false;
-          prevBlockTag ||= isHTMLElement(childNode) && BLOCK_TAGS.includes(childNode.tagName);
-          prevBr ||= isHTMLElement(childNode) && childNode.tagName === 'BR';
-
-          // console.log('prevBlockTag', node.tagName, prevBlockTag);
-
-          const result = {
-            node: childNode,
-            block: !isRoot && (isBlockByParent || isPrevBlockTag || isPrevBr),
-            root: isRoot,
-            text: isTextNode(childNode),
-            // meta: {
-            //   isRoot,
-            //   isBlockByParent,
-            //   isPrevBlockTag,
-            //   isPrevBr,
-            //   siblings: node.childNodes,
-            // },
-          };
-          // console.log(result, (childNode as any)?.outerHTML);
-          return result;
-        });
-
-        if (!childNodes.length) {
-          return [
-            {
-              node: document.createTextNode(''),
-              block: !root && block,
-              root,
-              text: true,
-            }
-          ];
+        if (isPre) {
+          return getChildNodesByPre(editorNode);
+        } else {
+          return getChildNodesByWithoutPre(editorNode);
         }
-
-        return childNodes;
       }
     }
   );
