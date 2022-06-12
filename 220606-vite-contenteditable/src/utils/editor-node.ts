dd12fe5d461822baabc3532da741f504c1d03abd
Low Front <lowfronterior@gmail.com>
2022-06-12 11:25:45 +0900

    add: paste parse

---
+++ 220606-vite-contenteditable/src/utils/editor-node.ts
@@ -0,0 +1,50 @@
+import { createDFSIterable } from "../helper/utils";
+
+/*
+- 파싱은 DFS(깊이우선) 순서로 진행한다. (첫번째 자식을 타고 확인해야하는 조건이 많음)
+- Root 노드는 block(줄넘김) 속성을 갖지 않는다.
+- Root 노드의 block 방지 속성은 첫번째 자식 노드에게 재귀적으로 전달된다.
+- Block(줄넘김) 속성을 갖고 있는 노드는 첫번째 자식 노드에게 재귀적으로 전달된다.
+- Block 노드의 block 속성은 가장 끝에 있는 자식 노드로 BR 태그를 만나면 무효화(줄넘김 방지) 된다.
+- BR 태그는 트리파싱의 다음 순서 노드에게 block 속성을 부여한다.
+- Block 속성의 태그는 다음 순서 노드에게 block 속성을 부여한다.
+
+<span>
+  123
+  <div>
+    abc
+    <span>def</span>
+  </div>
+  456
+</span>
+
+*/
+
+export const BLOCK_TAGS = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'ADDRESS', 'ARTICLE', 'ASIDE', 'BLOCKQUOTE', 'CANVAS', 'DD', 'DIV', 'DL', 'DT', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FOOTER', 'FORM', 'HEADER', 'HR', 'LI', 'MAIN', 'NAV', 'NOSCRIPT', 'OL', 'P', 'PRE', 'SECTION', 'TABLE', 'TFOOT', 'UL', 'VIDEO'];
+export const START_FRAGMENT = '<!--StartFragment-->';
+export const END_FRAGMENT = '<!--EndFragment-->';
+
+export type EditorNode = {
+  node: Node;
+  block: boolean;
+  root: boolean;
+  text: boolean;
+}
+
+export function isHTMLElement(node: Node): node is HTMLElement {
+  return node.nodeType === 1;
+} 
+
+export function isTextNode(node: Node) {
+  return node.nodeType === 3;
+}
+
+export function pasteParse(str: string) {
+  const startIndex = str.indexOf(START_FRAGMENT) + START_FRAGMENT.length;
+  const endIndex = str.indexOf(END_FRAGMENT);
+  
+  const wrap = document.createElement('div');
+  wrap.innerHTML = str.slice(startIndex, endIndex);
+
+  return wrap;
+}
