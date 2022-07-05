59b25aa6479027c5828e5bc08422b9c207bb23a0
Low Front <lowfronterior@gmail.com>
2022-07-06 01:45:42 +0900

    update

--- 220704-wysiwyg-editor/src/main.tsx
+++ 220704-wysiwyg-editor/src/main.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
 import "./index.css";
-import { WysiwygEditor } from "./WysiwygEditor";
+import WysiwygEditor from "./WysiwygEditor";
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(
 //   <React.StrictMode>
@@ -10,4 +10,5 @@ import { WysiwygEditor } from "./WysiwygEditor";
 //   </React.StrictMode>
 // )
 
-const wysiwygEditor = new WysiwygEditor(document.getElementById("root")!);
+// const wysiwygEditor = new WysiwygEditor(document.getElementById("root")!);
+WysiwygEditor.create(document.getElementById("root")!);
