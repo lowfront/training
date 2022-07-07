300d3df67389e32774a7e6bcea80c5b15934ee4f
Low Front <lowfronterior@gmail.com>
2022-07-07 23:09:11 +0900

    update

--- 220704-wysiwyg-editor/src/WysiwygEditor/Editor.ts
+++ 220704-wysiwyg-editor/src/WysiwygEditor/Editor.ts
@@ -1,4 +1,4 @@
-import { isHTML } from "./utils";
+import { getChildNodes, isHTML } from "./utils";
 
 namespace Editor {
   export function getSelection() {
@@ -12,10 +12,6 @@ namespace Editor {
     range.collapse(true);
     selection.removeAllRanges();
     selection.addRange(range);
-  }
-
-  export function getChildNodes(input: HTMLElement | Node) {
-    return [...input.childNodes];
   }
 
   export function appendParagraph(
@@ -39,34 +35,6 @@ namespace Editor {
   }
   export function indexOf(input: HTMLElement, node: Node) {
     return getChildNodes(input).indexOf(node as ChildNode);
-  }
-
-  export function isLastParagraph(
-    input: HTMLElement,
-    node: Node,
-    offset: number
-  ) {
-    if (input === node) {
-      const childNodes = getChildNodes(input);
-      return {
-        isLast: childNodes.length - 1 === offset,
-        paragraph: childNodes[offset],
-      };
-    }
-
-    let targetNode = node;
-    let isLast = true;
-
-    while (!isHTML(targetNode, "p")) {
-      const childNodes = getChildNodes(targetNode.parentNode!);
-      isLast &&= childNodes[childNodes.length - 1] === targetNode;
-      targetNode = targetNode.parentNode!;
-    }
-
-    return {
-      isLast,
-      paragraph: targetNode,
-    };
   }
 }
 
