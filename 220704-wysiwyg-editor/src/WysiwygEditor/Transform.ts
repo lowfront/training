b072e8a5324a56b7af75e8a7b119db347fb1d538
Low Front <lowfronterior@gmail.com>
2022-07-08 01:44:47 +0900

    update: enterTransform

--- 220704-wysiwyg-editor/src/WysiwygEditor/Transform.ts
+++ 220704-wysiwyg-editor/src/WysiwygEditor/Transform.ts
@@ -1,6 +1,6 @@
 import Editor from "./Editor";
 import Parser from "./Parser";
-import { getLastChildNode } from "./utils";
+import { getChildNodes, getLastChildNode, isText } from "./utils";
 
 namespace Transform {
   export function enterTransform(input: HTMLElement, ev: KeyboardEvent) {
@@ -11,25 +11,55 @@ namespace Transform {
 
     let focusNode: Node = input;
     let focusOffset: number = -1;
+
+    console.log(anchorNode, anchorOffset);
+
     if (anchorNode === input && !input.firstChild) {
-      // FirstEnter
+      // Enter in first
       Editor.appendParagraph(input);
       Editor.appendParagraph(input);
       focusOffset = 1;
+    } else if (Parser.isLast(input, anchorNode!, anchorOffset)) {
+      // Enter in end
+      const lastParagraph = getLastChildNode(input);
+      const paragraphIndex = Editor.indexOf(input, lastParagraph);
+      Editor.appendParagraph(input, "", paragraphIndex + 1);
+      focusOffset = paragraphIndex + 1;
     } else {
-      // console.log(anchorNode, anchorOffset);
+      // Enter in middle
+      const currentParagraph = Parser.getCurrentParagraph(input, anchorNode!);
+      const currentParagraphIndex =
+        getChildNodes(input).indexOf(currentParagraph);
+      const newParagraphIndex = currentParagraphIndex + 1;
 
-      const isLast = Parser.isLast(input, anchorNode!, anchorOffset);
+      if (isText(anchorNode!)) {
+        const splitedText = anchorNode.splitText(anchorOffset);
+        const newParagraph = Editor.appendParagraph(
+          input,
+          splitedText.length ? splitedText : "",
+          newParagraphIndex
+        );
 
-      if (isLast) {
-        // EndOfParagraph
-        const lastParagraph = getLastChildNode(input);
-        const paragraphIndex = Editor.indexOf(input, lastParagraph);
-        Editor.appendParagraph(input, "", paragraphIndex + 1);
-        focusOffset = paragraphIndex + 1;
-      }
+        if (!anchorNode.length) {
+          currentParagraph.appendChild(document.createElement("br"));
+        }
 
-      if (anchorNode!.nodeType === 3) {
+        if (splitedText.length) {
+          focusNode = splitedText;
+          focusOffset = 0;
+        } else {
+          focusNode = newParagraph;
+          focusOffset = 0;
+        }
+      } else {
+        console.log("not text");
+        const newParagraph = Editor.appendParagraph(
+          input,
+          "",
+          newParagraphIndex
+        );
+        focusNode = newParagraph;
+        focusOffset = 0;
       }
     }
 
