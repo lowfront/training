4c28f4092103095df108080d0532182237dc9cb5
Low Front <lowfronterior@gmail.com>
2022-07-13 23:18:47 +0900

    update

--- 220704-wysiwyg-editor/src/WysiwygEditor/Transform.ts
+++ 220704-wysiwyg-editor/src/WysiwygEditor/Transform.ts
@@ -65,57 +65,7 @@ namespace Transform {
     let focusNode: Node = input;
     let focusOffset: number = -1;
 
-    // console.log(anchorNode, anchorOffset);
-
-    if (anchorNode === input && !input.firstChild) {
-      // Enter in first
-      Editor.appendParagraph(input);
-      Editor.appendParagraph(input);
-      focusOffset = 1;
-    } else if (Parser.isLast(input, anchorNode!, anchorOffset)) {
-      // Enter in end
-      const lastParagraph = getLastChildNode(input);
-      const paragraphIndex = Editor.indexOf(input, lastParagraph);
-      Editor.appendParagraph(input, "", paragraphIndex + 1);
-      focusOffset = paragraphIndex + 1;
-    } else {
-      // Enter in middle
-      const currentParagraph = Parser.getCurrentParagraph(input, anchorNode!);
-      const currentParagraphIndex =
-        getChildNodes(input).indexOf(currentParagraph);
-      const newParagraphIndex = currentParagraphIndex + 1;
-
-      if (isText(anchorNode!)) {
-        const splitedText = anchorNode.splitText(anchorOffset);
-        const newParagraph = Editor.appendParagraph(
-          input,
-          splitedText.length ? splitedText : "",
-          newParagraphIndex
-        );
-
-        if (!anchorNode.length) {
-          currentParagraph.appendChild(document.createElement("br"));
-        }
-
-        if (splitedText.length) {
-          focusNode = splitedText;
-          focusOffset = 0;
-        } else {
-          focusNode = newParagraph;
-          focusOffset = 0;
-        }
-      } else {
-        const newParagraph = Editor.appendParagraph(
-          input,
-          "",
-          newParagraphIndex
-        );
-        focusNode = newParagraph;
-        focusOffset = 0;
-      }
-    }
-
-    Editor.focus(focusNode, focusOffset);
+    /* a tag cleanup after enter */
   }
 
   export function deleteTransform(input: HTMLElement, ev: InputEvent) {
