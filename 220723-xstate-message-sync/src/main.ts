908e989d1474ae0a3c05aa6d3cf97c374bad31c8
Low Front <lowfronterior@gmail.com>
2022-07-24 21:05:39 +0900

    add: ServiceConnector

--- 220723-xstate-message-sync/src/main.ts
+++ 220723-xstate-message-sync/src/main.ts
@@ -27,15 +27,26 @@ const messageCommunicationMachine = createMachine<typeof messageCommunicationCon
           },
         },
         NEW_FIELD: {
-          actions: assign((ctx) => {
-            return {
-              ...ctx,
-              object: {
-                ...ctx.object,
-                newField: {},
-              },
-            }
-          }),
+          actions: [
+            assign((ctx) => {
+              return {
+                ...ctx,
+                object: {
+                  ...ctx.object,
+                  newField: {},
+                },
+              }
+            }),
+            assign((ctx) => {
+              return {
+                ...ctx,
+                object: {
+                  ...ctx.object,
+                  newField2: {},
+                },
+              }
+            }),
+          ]
         }
       }
     }
@@ -60,4 +71,71 @@ function connectService(service: Interpreter<any, any, any, any, any>) {
   });
 }
 
-connectService(messageCommunicationService);
+class SerivceConnector {
+  mapServiceToPreviousState = new Map<
+    Interpreter<any, any, any, any, any>,
+    { 
+      state: State<any, any, any, any, any>,
+      context: any,
+    } | null
+  >();
+
+  mapServiceToListeningFields = new Map<
+    Interpreter<any, any, any, any, any>,
+    Set<string>
+  >();
+
+  connect(service: Interpreter<any, any, any, any, any>) {
+    service.onTransition((state) => {
+      this.diffContext(service, state);
+      this.mapServiceToPreviousState.set(service, {
+        state,
+        context: state.context,
+      });
+    });
+    this.mapServiceToPreviousState.set(service, null);
+    this.mapServiceToListeningFields.set(service, new Set<string>());
+  }
+
+  addListeningFields(service: Interpreter<any, any, any, any, any>, ...fields: string[]) {
+    const fieldsSet = this.mapServiceToListeningFields.get(service)
+    if (!fieldsSet) throw new Error(`Not exists service: ${service}`);
+    fields.forEach(field => fieldsSet.add(field));
+  }
+
+  private diffContext(service: Interpreter<any, any, any, any, any>, newState: State<any, any, any, any, any>) {
+    const fieldsSet = this.mapServiceToListeningFields.get(service)
+    if (!fieldsSet) throw new Error(`Not exists service: ${service}`);
+
+    const previousState = this.mapServiceToPreviousState.get(service);
+    if (!previousState) throw new Error(`Not initialized servcie: ${service}`);
+
+    const listeningKeys = [...fieldsSet].map(str => str.split('.'));
+
+    listeningKeys.forEach(keys => {
+      const {
+        previousValue,
+        newValue,
+      } = keys.reduce((acc, key) => {
+        return {
+          previousValue: acc.previousValue[key],
+          newValue: acc.newValue[key],
+        };
+      }, {
+        previousValue: previousState.context,
+        newValue: newState.context,
+      });
+      
+      if (previousValue !== newValue) {
+        if (typeof previousValue === 'object' && previousValue !== null && typeof newValue === 'object' && newValue !== null) {
+          // message send
+        } else {
+          // just send
+        }
+      }
+    });
+  }
+}
+
+const serviceConnector = new SerivceConnector();
+serviceConnector.connect(messageCommunicationService);
